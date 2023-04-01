609cf6e73cde17a89e8c5776de7bf356ef7abedf
rickythediver <wooseok1995@gmail.com>
2023-04-02 01:38:15 +0900

    [Add] BackGround & CameraShake function
    1) Added appropriate background for Title scene(couldn't find the exact one)
    2) Added Camera Shake function when mouse input is deteced

--- SANABI PROJECT/Assets/Scripts/UI/UIButtonController.cs
+++ SANABI PROJECT/Assets/Scripts/UI/UIButtonController.cs
@@ -8,10 +8,18 @@ using UnityEngine.UI;
 
 public class UIButtonController : MonoBehaviour, IPointerClickHandler
 {
+    Camera mainCamera;
     public int sceneID { get; set; }
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        Object.DontDestroyOnLoad(mainCamera); // 화면 흔들림은 계속 필요해서 매씬 데려갈꺼임
+
+        if (sceneID == (int)SceneNumber.Settings)
+        {
+            SceneManager.LoadScene(sceneID, LoadSceneMode.Additive);
+            return;
+        }
         if (sceneID == (int)SceneNumber.Exit)
         {
             Application.Quit(); // 게임을 종료하는 코드
@@ -24,6 +32,7 @@ public class UIButtonController : MonoBehaviour, IPointerClickHandler
     private void Start()
     {
         transform.GetChild(0).AddComponent<UITextController>();
+        mainCamera= Camera.main;
     }
 
     
